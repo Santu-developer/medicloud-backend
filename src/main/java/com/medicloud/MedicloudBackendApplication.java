a405b4292622eaea9a958bb54bf914f3a97693c6
Santu-developer <santoshpatidar.dev@gmail.com>
2026-02-28 19:31:42 +0530

    Initial Project Setup done

---
+++ src/main/java/com/medicloud/MedicloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicloudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicloud/MedicloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicloudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
