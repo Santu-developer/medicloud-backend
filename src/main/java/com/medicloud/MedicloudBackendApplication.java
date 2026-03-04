e2984813efbb7c503bebb63bfd322b08e9878ca2
Santu-developer <santoshpatidar.dev@gmail.com>
2026-03-04 22:18:59 +0530

    feat: project setup - Spring Boot 3.5.11, Java 21, dependencies, application config

--- src/main/java/com/medicloud/MedicloudBackendApplication.java
+++ src/main/java/com/medicloud/MedicloudBackendApplication.java
@@ -1,13 +1,17 @@
 package com.medicloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedicloudBackendApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(MedicloudBackendApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MedicloudBackendApplication.class, args);
+		log.info("MediCloud Backend started successfully");
 	}
-
 }
